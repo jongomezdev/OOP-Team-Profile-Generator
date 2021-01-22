c629720efd92a5b767ee1e0893745ec51bcfcece
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-22 11:20:58 -0500

    require path module and write pseudo code

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const path = require("path");
+
+//Employee parent class with the following properties: name, id and hte following methods: getName(), getId(), getEmail(), getRole()
+
+//in addition to Employee's properties and methods, Manager will also have the following: officeNumber, getRole() overidden to return manager
+
+//In addition to employee's properties and methods, engineer will also have the following: github username, getGithub() getRole() overidden to return engineer
+
+// in addition to employees properties and methods intern will also have the following: school getSchool() getRole() overidden to return Intern
