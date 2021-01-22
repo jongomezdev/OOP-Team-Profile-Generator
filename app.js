5d5262f22906cac2f97f97aec0e05b7d31234d40
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-22 12:20:05 -0500

    Test ID

--- app.js
+++ app.js
@@ -7,11 +7,24 @@ let team = [];
 async function genEmployee() {
   const managerInfo = await inquirer.prompt([
     {
+      type: "input",
       name: "name",
       message: "What is the team manager's name?",
       validate: async function (input) {
         if (input.length <= 0) return "Please enter a name";
         return true;
+      },
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the team manager's id?",
+      validate: async function (input) {
+        if (input > 0) {
+          return true;
+        } else {
+          return "Please enter a NUMBER greater than zero";
+        }
       },
     },
   ]);
