42b72f81d5a8791f21f6c9a83839a1a34beab378
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-22 12:00:21 -0500

    Add first question with validation

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 
+let team = [];
+
+async function genEmployee() {
+  const managerInfo = await inquirer.prompt([
+    {
+      name: "name",
+      message: "What is the team manager's name?",
+      validate: async function (input) {
+        if (input.length <= 0) return "Please enter a name";
+        return true;
+      },
+    },
+  ]);
+}
+
+genEmployee();
 //Employee parent class with the following properties: name, id and hte following methods: getName(), getId(), getEmail(), getRole()
 
 //in addition to Employee's properties and methods, Manager will also have the following: officeNumber, getRole() overidden to return manager
 
 //In addition to employee's properties and methods, engineer will also have the following: github username, getGithub() getRole() overidden to return engineer
 
-// in addition to employees properties and methods intern will also have the following: school getSchool() getRole() overidden to return Intern
+// in addition to employees properties and methods intern will also have the following: school getSchool() getRole() overidden to return inter
