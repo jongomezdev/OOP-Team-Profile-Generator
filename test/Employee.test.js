e58f8fb6d145b1cbe7c9250f263ca3d3851b3a69
Jonathan Gomez <jongomezdev@gmail.com>
2021-01-24 18:04:44 -0500

    test for name and id

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,1 +1,18 @@
 const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee case", () => {
+  const e = new Employee();
+  expect(typeof e).toBe("object");
+});
+
+test("Can set a name via constructor arguments", () => {
+  const name = "Jonathan";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
+
+test("Can test id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Jargin", testValue);
+  expect(e.id).toBe(testValue);
+});
